3c192f26f436722363dc8a0af5bf64b29bf368eb
timotheleduc0806 <ledt02@uqo.ca>
2025-10-08 12:28:35 -0400

    Initial commit - Maven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testAddition() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
